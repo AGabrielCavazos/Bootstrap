c693f6f48ac87fcd10d59f648e241db2920dddce
Gabriel Cavazos <angolo501@gmail.com>
2018-08-10 23:31:23 -0500

    Practica 1 agregada

---
+++ Plantilla.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<title>plantilla bootstrap</title>
+</head>
+	 
+<body>
+
+
+<!-- Columna Tamaños:
+.col      Extra Pequeño (Extra small) - smartphones Vertical  Menos de 544px
+.col-sm   Pequeño (Small) - Smartphones Verticual             Mas de 544px y menos de 768px
+.col-md   Mediano (Medium) - Tables						      Mas de 768px y Menos de 992px
+.col-lg	  Largo(large) - Computadoras						  Mas de 992 px y menos de 1200px
+.col-xl   Extra Largo (Extra Large) - Computadoras			  Mas de 1200px
+-->
+
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+		<h1>hola mundo</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+
+		<div class="col-12 col-md-6">
+		<h1>hola mundo</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+
+				<div class="col-12 col-md-6">
+		<h1>hola mundo</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+
+	</div>
+</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
